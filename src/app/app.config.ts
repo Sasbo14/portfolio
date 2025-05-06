ad772f66cbfa1a65ad7f80322eab8d6428d0aeae
Sasbo14 <larbi.ch@hotmail.fr>
2025-05-06 15:59:40 +0200

    Update app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
 import { withHashLocation } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withHashLocation()),
     provideClientHydration(withEventReplay()),
+    { provide: APP_BASE_HREF, useValue: '/portfolio/' },
   ],
 };
