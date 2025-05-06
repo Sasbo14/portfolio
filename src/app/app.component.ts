51b3a98b98ae6f9a8b21b4f08a9adf501ddb4c9b
Sasbo14 <larbi.ch@hotmail.fr>
2025-05-06 14:22:15 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router'; // Ajouté
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true, // <-- Ajouté
+  imports: [RouterModule, MatToolbarModule, MatButtonModule],
+  template: `
+    <!-- Remplacez templateUrl par template -->
+    <mat-toolbar color="primary">
+      <span>Mon Portfolio</span>
+      <nav>
+        <a mat-button routerLink="/">Accueil</a>
+        <a mat-button routerLink="/projects">Projets</a>
+        <a mat-button routerLink="/contact">Contact</a>
+      </nav>
+    </mat-toolbar>
+    <router-outlet></router-outlet>
+  `,
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio-angular';
